45b8e464adf0a081b2a1318a4f4672b93c42549c
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-01-15 09:19:00 +0100

    created the database connection and the databse instanse stell need some work

---
+++ db.sql
@@ -0,0 +1,47 @@
+create database youdemy;
+use youdemy;
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    user_type ENUM('Student', 'Teacher', 'Administrator') NOT NULL
+);
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE Course (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE Course_Category (
+	id int primary key auto_increment,
+    course_id INT,
+    category_id INT,
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
+);
+CREATE TABLE Course_Tag (
+	id int primary key auto_increment,
+    course_id INT,
+    tag_id INT,
+    CONSTRAINT fk_course_tag FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
+);
+CREATE TABLE Enrollment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    enrollment_date DATE NOT NULL,
+    status ENUM('Active', 'Completed', 'Canceled') NOT NULL,
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES User(id) ON DELETE CASCADE,
+    CONSTRAINT fk_course_enrollment FOREIGN KEY (course_id) REFERENCES Course(id) ON DELETE CASCADE
+);
